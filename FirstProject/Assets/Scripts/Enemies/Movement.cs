b214c24a2e8ced04afd3114b36e4ae8fd1273f89
João Augusto Lima <figorifico3000@gmail.com>
2020-10-14 21:21:40 +0100

    Spawn and pathfind

---
+++ FirstProject/Assets/Scripts/Enemies/Movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Movement : MonoBehaviour
+{
+
+    private NavMeshAgent agent;
+
+    private Vector3 target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = new Vector3(0, 0.1f, 0);
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(target);
+    }
+}
---
+++ FirstProject/Assets/Scripts/GameController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    private float time;
+
+    public GameObject mainMarker;
+    public GameObject enemy1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if(time > 1f)
+        {
+            Vector2 circleRandom = Random.insideUnitCircle.normalized * Random.Range(10,15);
+            Vector3 pos = new Vector3(circleRandom.x, 0.1f, circleRandom.y);
+            var obj = Instantiate(enemy1, pos,Quaternion.identity);
+            obj.transform.SetParent(mainMarker.transform);
+            time = 0;
+        }
+    }
+}
