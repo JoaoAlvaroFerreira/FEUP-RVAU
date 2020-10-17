bbbd38c601b45d54a22107fe6400ca29aa300997
JoaoAlvaroFerreira <joaoalvaro.ferreira@hotmail.com>
2020-10-17 18:31:41 +0100

    enemies die now

---
+++ FirstProject/Assets/Scripts/Enemies/CollisionWithBullet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionWithBullet : MonoBehaviour
+{
+    public GameObject enemyInstance;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        Debug.Log(other.gameObject.tag);
+        Debug.Log("aaa");
+        if (other.gameObject.tag == "Bullet")
+        {
+            Destroy(other.gameObject);
+            Destroy(this.enemyInstance);
+            //or gameObject.SetActive(false);
+        }
+    }
+}
