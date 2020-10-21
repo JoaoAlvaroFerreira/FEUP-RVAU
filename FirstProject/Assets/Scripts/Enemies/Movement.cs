0167e22d9f5b3f7edfab81ffa4721ee4e0f0aff5
William Hjelm <william.hjelm@fe.up.pt>
2020-10-21 23:30:35 +0100

    enemies are now running knights

--- FirstProject/Assets/Scripts/Enemies/Movement.cs
+++ FirstProject/Assets/Scripts/Enemies/Movement.cs
@@ -8,17 +8,32 @@ public class Movement : MonoBehaviour
 
     private NavMeshAgent agent;
 
+
     private Vector3 target;
+
+    //private GameObject mainMarker;
+
+    private Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
         target = new Vector3(0, 0.1f, 0);
         agent = GetComponent<NavMeshAgent>();
+        anim.SetBool("isCharging", true);
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        // mainMarker = GameObject.FindWithTag("MainMarker");
+
+        // if(mainMarker.GetComponent<MainMarker>().EnteredTrigger)
+        // {
+
+        // }
+
         agent.SetDestination(target);
     }
 }
