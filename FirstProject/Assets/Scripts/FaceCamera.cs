1356d061886e7ba31a36c17fe44d0a5853436784
João Augusto Lima <figorifico3000@gmail.com>
2020-11-02 01:07:31 +0000

    Half sphere

---
+++ FirstProject/Assets/Scripts/FaceCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceCamera : MonoBehaviour
+{
+
+    public new Camera camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var n = camera.transform.position;
+        transform.rotation = Quaternion.LookRotation(n) * Quaternion.Euler(0, 0, 180);
+    }
+}
